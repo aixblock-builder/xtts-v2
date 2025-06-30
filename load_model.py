552ae1acbb19ee92abb90137350590b05cbc93fc
tqphu27 <trn.quangph27@gmail.com>
2025-06-30 10:28:28 +0700

    .

--- load_model.py
+++ load_model.py
@@ -5,6 +5,4 @@ from TTS.api import TTS
 # Auto accept Coqui ToS
 builtins.input = lambda _: "y"
 
-assert os.path.isfile("male.wav"), "❌ 'male.wav' not found!"
-
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
